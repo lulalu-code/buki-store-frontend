c3d744957587d626d655895923eee836e83de054
lulalu-code <lucialazarogarrido@gmail.com>
2023-11-08 23:40:28 +0100

    First commit

---
+++ src/app/User/contact-form/contact-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './User/register/register.component';
+import { ProfileComponent } from './User/profile/profile.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { HeaderComponent } from './Shared/header/header.component';
+import { ContactFormComponent } from './User/contact-form/contact-form.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { ProductFormComponent } from './Product/product-form/product-form.component';
+import { ProductListComponent } from './Product/product-list/product-list.component';
+import { ProductCardComponent } from './Product/product-card/product-card.component';
+import { SearchBarComponent } from './Product/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ProfileComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactFormComponent,
+    AboutUsComponent,
+    LoginComponent,
+    ProductFormComponent,
+    ProductListComponent,
+    ProductCardComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
