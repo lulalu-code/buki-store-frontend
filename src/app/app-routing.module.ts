9172150a2ced3f2adf9ace7633fac0d7d5b0609f
lulalu-code <lucialazarogarrido@gmail.com>
2023-12-03 21:13:28 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Home/home/home.component';
+import { RegisterComponent } from './User/register/register.component';
+import { ProfileComponent } from './User/profile/profile.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductFormComponent } from './Product/product-form/product-form.component';
+import { ProductCardComponent } from './Product/product-card/product-card.component';
+import { ContactFormComponent } from './User/contact-form/contact-form.component';
+import { LoginComponent } from './Auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'about', component: AboutUsComponent},
+  { path: 'product', component: ProductCardComponent},
+  { path: 'contact', component: ContactFormComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'product-form', component: ProductFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './Shared/footer/footer.component';
-import { HeaderComponent } from './Shared/header/header.component';
+import { FooterComponent } from './Home/footer/footer.component';
+import { HeaderComponent } from './Home/header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { LoginComponent } from './Auth/login/login.component';
 import { ProductFormComponent } from './Product/product-form/product-form.component';
@@ -11,6 +11,10 @@ import { ProductListComponent } from './Product/product-list/product-list.compon
 import { ProductCardComponent } from './Product/product-card/product-card.component';
 import { SearchBarComponent } from './Product/search-bar/search-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactFormComponent } from './User/contact-form/contact-form.component';
+import { ProfileComponent } from './User/profile/profile.component';
+import { RegisterComponent } from './User/register/register.component';
+import { HomeComponent } from './Home/home/home.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ProductFormComponent,
     ProductListComponent,
     ProductCardComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ContactFormComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
