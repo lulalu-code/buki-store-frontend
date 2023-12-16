3d5de6b9b1b8b7f7ad092bb8ca921e01b1781cda
lulalu-code <lucialazarogarrido@gmail.com>
2023-12-16 17:22:37 +0100

    Product list creation

--- src/app/components/Product/product-list/product-list.component.ts
+++ src/app/components/Product/product-list/product-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.sass']
 })
 export class ProductListComponent {
+  products: Product[] = [];
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,){}
 
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+  getAllProducts() {
+    return  this.productService
+      .getAllProducts()
+      .subscribe((products) => {
+        console.log("products is:" + JSON.stringify(products))
+        this.products = products;
+      });
+    }
 }
