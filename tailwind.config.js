58f11f99b63dbb63e24fa796385d8c96161e4224
lulalu-code <lucialazarogarrido@gmail.com>
2023-12-26 13:43:58 +0100

    Adding responsive features

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,17 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  theme: {
+    screens: {
+      'tablet': '540px',
+      // => @media (min-width: 540px) { ... }
+
+      'laptop': '1200px',
+      // => @media (min-width: 1300px) { ... }
+
+      'desktop': '1500px',
+      // => @media (min-width: 1500px) { ... }
+    },
+  }
 }
 
