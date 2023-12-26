90dbad21ff161104cabd025214677237976dbfca
lulalu-code <lucialazarogarrido@gmail.com>
2023-12-26 23:06:28 +0100

    Adding about-us component

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       // => @media (min-width: 540px) { ... }
 
       'laptop': '1200px',
-      // => @media (min-width: 1300px) { ... }
+      // => @media (min-width: 1200px) { ... }
 
       'desktop': '1500px',
       // => @media (min-width: 1500px) { ... }
